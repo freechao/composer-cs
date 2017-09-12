164b586468319f2965f40272812e9f9efcb8c778
mashuchao <mashuchao@yunzongnet.com>
2017-09-12 15:03:28 +0800

    提交测试

---
+++ model/Student.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ford;
+
+/**
+ * Created by PhpStorm.
+ * User: mashuchao
+ * Date: 2017/9/12
+ * Time: 下午3:00
+ */
+class Student
+{
+    public static function test(){
+        echo '你好';
+    }
+}
